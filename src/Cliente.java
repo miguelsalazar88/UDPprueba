6ce079cf379fe7a7b50cd6d72ab3366038ba9a4f
Miguel Salazar <miguelsalazar88@gmail.com>
2022-02-17 19:31:42 -0500

    Se agragan todos los comentarios

--- src/Cliente.java
+++ src/Cliente.java
@@ -5,59 +5,78 @@ import java.io.IOException;
 import java.net.*;
 import java.util.Scanner;
 
+
+//Clase Cliente: Cuenta con una ventana para enviar mensajes y confirmar recepción por parte
+//del servidor.
 public class Cliente extends JFrame implements ActionListener {
 
     //Elementos del Protocolo UDP
-    private DatagramSocket datagramSocket;
-    private InetAddress inetAddress;
-    private byte[] buffer;
+    private DatagramSocket datagramSocket; //Instancia de socket para el envío de datos.
+    private InetAddress inetAddress; // Instancia de Dirección IP
+    private byte[] buffer; // Buffer de envío de datos para el protocolo UDP
 
     //Elementos de la Ventana
-    private JLabel labelCliente = new JLabel("Cliente:");
-    private JButton botonEnviar = new JButton("Enviar");
-    private JTextArea textoCliente = new JTextArea();
-    private JScrollPane scrollPaneTexto = new JScrollPane(textoCliente);
-    private JTextField JTFCliente = new JTextField();
+    private JLabel labelCliente = new JLabel("Cliente:"); //Label del JTArea
+    private JButton botonEnviar = new JButton("Enviar"); // Boton para enviar mensaje
+    private JTextArea textoCliente = new JTextArea(); //JTArea para mostrar el contenido del mensaje
+    private JScrollPane scrollPaneTexto = new JScrollPane(textoCliente); // Scroll Pane para el JTA
+    private JTextField JTFCliente = new JTextField(); //JTF para escribir el mensaje que se va a enviar.
 
-
+    //Constructor del Objeto CLiente
     public Cliente(DatagramSocket datagramSocket, InetAddress inetAddress) {
 
         //UDP
-        this.datagramSocket = datagramSocket;
-        this.inetAddress = inetAddress;
+        this.datagramSocket = datagramSocket; //Se asigna el socket a la variable
+        this.inetAddress = inetAddress; // Se asigna la dirección IP a la variable
 
         //Ventana
-        this.setSize(500,500);
-        this.setVisible(true);
-        this.setLayout(null);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setTitle("Ventana Cliente");
-        this.initComponents();
+        this.setSize(500,500); //Tamaño de la ventana
+        this.setVisible(true); // Visibilidad de la ventana
+        this.setLayout(null); // Sin layout
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE); //Se termina el programa cuando se cierra la ventana
+        this.setTitle("Ventana Cliente"); // Título de la ventana
+        this.initComponents(); //Método que inicia los componentes del JFrame
 
     }
 
     private void initComponents() {
-        this.add(labelCliente);
-        this.labelCliente.setBounds(10,10,70,30);
-        this.add(scrollPaneTexto);
-        this.textoCliente.setEditable(false);
-        this.scrollPaneTexto.setBounds(10,50,300,300);
-        this.add(JTFCliente);
-        this.JTFCliente.setBounds(10,360,300,30);
-        this.add(botonEnviar);
-        this.botonEnviar.setBounds(10,400,100,30);
-        this.botonEnviar.addActionListener(this);
+        this.add(labelCliente); //Label del JTA
+        this.labelCliente.setBounds(10,10,70,30); //Tamaño del label
+        this.add(scrollPaneTexto); // Scroll pane para el JTA y poder ver todo el texto
+        this.textoCliente.setEditable(false); // Se deja el JTA sin opción de editar
+        this.scrollPaneTexto.setBounds(10,50,300,300); // Tamaño del JTA
+        this.add(JTFCliente); //JTexto Field para escribir el contenido del mensaje
+        this.JTFCliente.setBounds(10,360,300,30); // Tamaño del JTF
+        this.add(botonEnviar); // Boton para enviar el mensaje
+        this.botonEnviar.setBounds(10,400,100,30); //Tamaño del boton enviar
+        this.botonEnviar.addActionListener(this); //Se agrega un Actionlistener al boton enviar
 
     }
 
+    //Este metodo convierte el mensaje a Bytes y lo empaqueta para enviarlo al servidor por el
+    //puerto 1234
     public void enviarMensaje(){
             try{
-                String mensaje = this.JTFCliente.getText();
-                buffer = mensaje.getBytes();
+
+                String mensaje = this.JTFCliente.getText(); //Recibe el mensaje del JTF
+                buffer = mensaje.getBytes(); //Se convierte el mensaje a Bytes
+
+                //Se crea una instancia de DatagramPacket para poder encapsular el mensaje, y enviarlo por el puerto
+                // 1234
                 DatagramPacket datagramPacket = new DatagramPacket(buffer,buffer.length, inetAddress, 1234);
+
+                //Se envía el mensaje a través del Socket
                 datagramSocket.send(datagramPacket);
+
+
+                // Se espera la confirmación de la recepción del mensaje por parte del servidor
                 datagramSocket.receive(datagramPacket);
+
+                //Recibe el DatagramPacket que contiene la confirmación de recepción del mensaje por parte
+                //del servidor
                 String mensajeDelServidor = new String(datagramPacket.getData(),0,datagramPacket.getLength());
+
+                //Se actualiza el JTA para mostrar el mensaje recibido
                 this.actualizarTextoCliente(mensajeDelServidor);
             } catch (IOException e){
                 e.printStackTrace();
@@ -65,10 +84,13 @@ public class Cliente extends JFrame implements ActionListener {
 
     }
 
+    //Este metodo actualiza el JTextArea que contiene la confirmación de los mensajes enviados
+    //al servidor
     public void actualizarTextoCliente(String s){
         this.textoCliente.setText(this.textoCliente.getText() + "\n" + "El servidor recibió el mensaje: " + s);
     }
 
+    //Main
     public static void main(String[] args) throws SocketException, UnknownHostException {
         DatagramSocket datagramSocket = new DatagramSocket();
         InetAddress inetAddress = InetAddress.getByName("localhost");
@@ -76,12 +98,12 @@ public class Cliente extends JFrame implements ActionListener {
 
     }
 
+    //Método para ejecutar acciones cuando se presiona el botón de enviar el mensaje
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource().equals(this.botonEnviar)){
-            this.enviarMensaje();
-            this.JTFCliente.setText("");
-
+            this.enviarMensaje(); //Envía el mensaje
+            this.JTFCliente.setText(""); //Borra el contenido del JTF
         }
     }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -10,46 +10,51 @@ import java.net.SocketException;
 public class Servidor extends JFrame {
 
     //ELementos UDP
-    private DatagramSocket datagramSocket;
-    private byte[] buffer = new byte[256];
+    private DatagramSocket datagramSocket; //Instancia del socket
+    private byte[] buffer = new byte[256]; //Instancia para el buffer
 
     //Elementos Ventana
-    private JLabel labelServidor = new JLabel("Servidor:");
-    private JTextArea textoServidor = new JTextArea();
-    private JScrollPane scrollPaneTexto = new JScrollPane(textoServidor);
+    private JLabel labelServidor = new JLabel("Servidor:"); //Label del JTA
+    private JTextArea textoServidor = new JTextArea(); //JTA
+    private JScrollPane scrollPaneTexto = new JScrollPane(textoServidor); //Scroll Pane para el JTA
 
     public Servidor(DatagramSocket datagramSocket) {
         //UDP
-        this.datagramSocket = datagramSocket;
+        this.datagramSocket = datagramSocket; //Se asigna el DatagramSocket a la variable
 
         //Ventana
-        this.setSize(500,500);
-        this.setVisible(true);
-        this.setLayout(null);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setTitle("Ventana Servidor");
-        this.initComponents();
+        this.setSize(500,500); //Tamaño de la ventana
+        this.setVisible(true); // Visibilidad de la ventana
+        this.setLayout(null); // Sin layout
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE); //Se termina el programa al cerrar la ventana
+        this.setTitle("Ventana Servidor"); //Título de la ventana
+        this.initComponents(); //Inicializador de los componentes de la ventana
     }
 
     //Se inicializan los componrntes de la Ventana
     private void initComponents() {
-        this.add(labelServidor);
-        this.labelServidor.setBounds(10,10,70,30);
-        this.add(scrollPaneTexto);
-        this.textoServidor.setEditable(false);
-        this.scrollPaneTexto.setBounds(10,50,300,300);
+        this.add(labelServidor); // Label del JTA
+        this.labelServidor.setBounds(10,10,70,30); //Tamaño del label
+        this.add(scrollPaneTexto); // Scroll Pane del JTA
+        this.textoServidor.setEditable(false); //No se puede editar el JTA
+        this.scrollPaneTexto.setBounds(10,50,300,300); //Tamaño del JTA
 
     }
 
+    //Este metodo tiene como proposito recibir los mensajes por parte del servidor y responder
+    //con una confirmación que contiene el mensaje
     public void recibirMensaje(){
         while (true){
             try{
                 DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
                 datagramSocket.receive(datagramPacket);
-                InetAddress inetAddress = datagramPacket.getAddress();
-                int puerto = datagramPacket.getPort();
+                InetAddress inetAddress = datagramPacket.getAddress(); //Direccion IP del paquete
+                int puerto = datagramPacket.getPort(); //Recibe el numero del puerto del paquete
+                //Se asigna el contenido del mensaje a un String para mostrarlo en el JTA
                 String mensajeCliente = new String(datagramPacket.getData(), 0, datagramPacket.getLength());
+                //Actualiza el JTA para mostrar el nuevo mensaje recibido
                 this.actualizarTextoServidor(mensajeCliente);
+                //Confirmación de recepción del mensaje
                 datagramPacket = new DatagramPacket(buffer, buffer.length, inetAddress, puerto);
                 datagramSocket.send(datagramPacket);
             } catch (IOException e){
@@ -60,10 +65,11 @@ public class Servidor extends JFrame {
         }
     }
 
+    //Este metodo actualiza el JTA para mostrar el contenido de texto del mensaje
     public void actualizarTextoServidor(String s){
         this.textoServidor.setText(this.textoServidor.getText() + "\n" + "Mensaje del Cliente: " + s);
     }
-
+    //Main
     public static void main(String[] args) throws SocketException {
         DatagramSocket datagramSocket = new DatagramSocket(1234);
         Servidor servidor = new Servidor(datagramSocket);
